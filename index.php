21b70519da197823b1c151dd4ec0413c23673df6
Maximilien Le Pivain <maximilien.lepivain@ideesculture.com>
2017-08-18 10:29:20 +0200

    UI enhancement

--- index.php
+++ index.php
@@ -26,11 +26,15 @@
 
 <html>
 <head>
+
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
     <!-- Optional theme -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+  <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.4/css/bootstrap-select.min.css">
 
     <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.3.1/css/buttons.dataTables.min.css">
 
@@ -47,9 +51,9 @@
 </head>
 <body>
         
-
     <div class="container">
-        <h1>Ensemble des fichiers disponibles</h1>
+        <h1><img src="ideesculture.png" style="height: 48px;"/> <small>Analyse de données</small></h1>
+        <h3>Ensemble des fichiers disponibles</h3>
         <ul class="list-unstyled">
             <?php
                 //
@@ -57,29 +61,55 @@
 
                 foreach ($files as $key => $file) {
 
-                    $fileDir = dirname($file);
-                    $filename = basename($file);
+                    $fileDir  = dirname($file);
 
                     // Check if in the same file
                     if($previous == $fileDir) {
-                        echo '<li>';
-                        echo '<a href="analyze.php?file='.$file.'">'.$filename.'</a>';
-                        echo '</li>';
-                    } else  if ($previous != $fileDir){
-                        // End the previous list
+                        $filename = basename($file);
+                        displayFile($file);
+
+                    } else {
                         echo '</ul>';
-                        // Create a new one
-                        echo '<ul>';
-                        echo '<h1><small>'.$fileDir.'</small></h1>';
-                        echo '<li>';
-                        echo '<a href="analyze.php?file='.$file.'">'.$filename.'</a>';
-                        echo '</li>';
+                        echo '<ul class="list-unstyled list-group">';
+                        echo '<h3><small>'.$fileDir.'</small></h3>';
+                        displayFile($file);
                         $previous = $fileDir;
                     }
-
                 }
+
+                function displayFile($file) {
+                    $filename = basename($file);
+
+
+                    echo '<li class="list-group-item">';
+                    echo '<form>';
+                    echo '<a href="analyze.php?file='.$file.'">'.$filename.'</a>';
+                    echo '<select class="selectpicker show-menu-arrow pull-right " data-style="btn-primary" data-width="150px">
+                            <option data-icon="glyphicon glyphicon-ok-circle">Ok</option>
+                            <option data-icon="glyphicon-warning-sign">Warning</option>
+                            <option data-icon="glyphicon-ban-circle">Stop</option>
+                        </select>';
+                    echo '</form>';
+                    echo '</li>';
+                } 
             ?>
         </ul>
     </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.4/js/bootstrap-select.min.js"></script>
+
+    <script>
+        $('.selectpicker').selectpicker({
+          style: 'btn-primary',
+          size: 3
+        });
+    </script>
+
+
+
 </body>
 </html>
+
+
